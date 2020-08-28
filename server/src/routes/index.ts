bb5bce471d575d01f8ed35238d7aa6b83711746a
SELA <RiSEblackbird@users.noreply.github.com>
2020-08-28 14:56:41 +0900

    Update index.ts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { getTodos, addTodo, updateTodo, deleteTodo } from '../controllers/todos'
- 
+
 const router: Router = Router()
 
 router.get('/todos', getTodos)
